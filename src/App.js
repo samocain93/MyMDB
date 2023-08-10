2c0c1e4bb1d7a6a6545c926c45094940fae162aa
Sam O'Cain <samocain93@gmail.com>
2023-08-09 20:10:39 -0500

    still refactoring

--- src/App.js
+++ src/App.js
@@ -55,18 +55,24 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox movies={movies}>
+        <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
     <nav className='nav-bar'>
+      {children}
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
     </nav>
   );
 }
@@ -102,18 +108,15 @@ function Search() {
   );
 }
 
-function Main({ movies }) {
+function Main({ children }) {
   return (
     <>
-      <main className='main'>
-        <ListBox movies={movies} />
-        <WatchedBox />
-      </main>
+      <main className='main'>{children}</main>
     </>
   );
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -124,7 +127,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? '–' : '+'}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && {children}}
     </div>
   );
 }
